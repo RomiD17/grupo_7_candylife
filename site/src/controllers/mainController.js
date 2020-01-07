c8b7c95e026135609d9fe1373e15973d2b5aa0b5
Lusine <lusinesarafian@gmail.com>
2020-01-07 14:04:11 -0300

    correccio de product por producto

--- site/src/controllers/mainController.js
+++ site/src/controllers/mainController.js
@@ -1,29 +1,34 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 // ************ Function to Read an HTML File ************
-function readHTML (fileName) {
-	let filePath = path.join(__dirname, `/../views/${fileName}.html`);
-	let htmlFile = fs.readFileSync(filePath, 'utf-8');
-	return htmlFile;
+function readHTML(fileName) {
+  let filePath = path.join(__dirname, `/../views/${fileName}.html`);
+  let htmlFile = fs.readFileSync(filePath, "utf-8");
+  return htmlFile;
 }
 
+const ubicacionProductosJSON = path.join(__dirname, '../data/products.json');
+
+let contenidoProductosJSON = fs.readFileSync(ubicacionProductosJSON, 'utf-8');
+
 const controller = {
-	root: (req, res) => {
-		res.render('index');
-	},
-	productDetail: (req, res) => {
-		res.render('productDetail');
-	},
-	productCart: (req, res) => {
-		res.render('productCart');
-	},
-	register: (req, res) => {
-		res.render('register');
-	},
-	productAdd: (req, res) => {
-		res.render('productAdd');
-	},
+  root: (req, res) => {
+    let products = JSON.parse(contenidoProductosJSON);
+		res.render('index', { products });
+  },
+  productDetail: (req, res) => {
+    res.render("productDetail");
+  },
+  productCart: (req, res) => {
+    res.render("productCart");
+  },
+  register: (req, res) => {
+    res.render("register");
+  },
+  productAdd: (req, res) => {
+    res.render("productAdd");
+  },
 };
 
-module.exports = controller
+module.exports = controller;
