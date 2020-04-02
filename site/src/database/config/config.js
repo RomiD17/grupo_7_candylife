e6491df6f749701468f1a808f00b96a6d56efbc7
Romina Duarte <romduarte@MacBook-Pro-de-Romina.local>
2020-04-02 17:36:27 -0300

    Cambio config db

--- site/src/database/config/config.js
+++ site/src/database/config/config.js
@@ -1,14 +1,24 @@
 module.exports = 
 {
   "development": {
-    "username": "lupe",
-    "password": "lupe",
-    "database": "candylife_DB",
+    "username": "root",
+    "password": null,
+    "database": "candylife",
     "host": "127.0.0.1",
     "dialect": "mysql",
     "operatorsAliases": false,
     "port": 3309
   },
+  //Luchi
+  //"development": {
+  //   "username": "lupe",
+  //   "password": "lupe",
+  //   "database": "candylife_DB",
+  //   "host": "127.0.0.1",
+  //   "dialect": "mysql",
+  //   "operatorsAliases": false,
+  //   "port": 3309
+  // },
   "test": {
     "username": "root",
     "password": null,
